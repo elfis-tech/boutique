55b7b4b21f7baddaddf5c94cfb3a194606e00238
Valentin <valentin-fouilloud.com>
2025-05-13 17:32:28 +0200

    Presque Finit

--- app.js
+++ app.js
@@ -13,3 +13,27 @@ userBtn.addEventListener("click",()=> {
     searchContainer.classList.remove("active")
     userContent.classList.toggle("active")
 });
+
+var swiper = new Swiper(".news-content", {
+    spaceBetween: 100,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        568:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1020:{
+            slidesPerView:3,
+        },
+    },
+    });
